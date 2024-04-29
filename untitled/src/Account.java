91b26e0690fe15a3f7a081837ce0c46a8a989aab
SylarZzz <sylarzhang926@hotmail.com>
2024-04-29 19:07:55 -0400

    removed setters

--- untitled/src/Account.java
+++ untitled/src/Account.java
@@ -38,23 +38,12 @@ public class Account {
         return accountNumber;
     }
 
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
     public String getAccountHolder() {
         return accountHolder;
-    }
-
-    public void setAccountHolder(String accountHolder) {
-        this.accountHolder = accountHolder;
     }
 
     public double getBalance() {
         return balance;
     }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 }
